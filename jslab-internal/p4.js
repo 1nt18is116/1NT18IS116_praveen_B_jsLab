1c79d436f81069ea0781a9152ebc437aaaf03020
1nt18is116 <82606744+1nt18is116@users.noreply.github.com>
2021-07-11 17:36:01 +0530

    lab-internal files

---
+++ jslab-internal/p4.js
@@ -0,0 +1,25 @@
+// jshint esversion: 6
+
+const obj1 = [
+    {
+        dish: "Dosa",
+        
+    }
+
+    utpam : "utpam is a tasty south-Indian dish. <b>Advantages</b> it is tasty. <b>Disadvantages</b> it is oily "
+    };
+
+]
+
+
+function dish(){
+    choice1= String(prompt("Enter your dish"))
+    // choice2 = String(prompt("Enter any special condition"))
+    
+    
+    console.log(choice.toLowerCase())
+    document.getElementById("p4").innerHTML = jsonify(obj1[choice])
+}
+
+
+// var news = prompt("what?")
